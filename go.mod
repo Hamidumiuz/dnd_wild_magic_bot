cfeb2834e862d80e6ee89558bdd9fbfcc10bf25d
hamidumi <hamidumi@yandex.ru>
2025-11-25 10:06:19 +0500

    add echo bot

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/hamidumiuz/dnd_wild_magic_bot
+
+go 1.25.1
+
+require github.com/go-telegram/bot v1.17.0 // indirect
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+)
+
+func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	opts := []bot.Option{
+		bot.WithDefaultHandler(handler),
+	}
+
+	token := os.Getenv("TELEGRAM_BOT_TOKEN")
+	if len(token) == 0 {
+		log.Fatal("TELEGRAM_BOT_TOKEN environment variable is not set")
+	}
+	b, err := bot.New(token, opts...)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	b.Start(ctx)
+}
+
+func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: update.Message.Chat.ID,
+		Text:   update.Message.Text,
+	})
+}
